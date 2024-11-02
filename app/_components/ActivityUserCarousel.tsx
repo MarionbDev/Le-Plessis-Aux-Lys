5397a156a5af8260e2b0a27b801826aa52b6c4bf
MarionbDev <marionbaston84@gmail.com>
2024-11-02 14:03:54 +0100

    update styles responsive

--- app/_components/ActivityUserCarousel.tsx
+++ app/_components/ActivityUserCarousel.tsx
@@ -22,7 +22,8 @@ export default function ActivityUserCarousel({
   return (
     <>
       {slides.length > 0 && (
-        <section className="flex w-[21rem] xl:w-[44rem] pt-[2rem] xl:pt-[4.5rem] ">
+        // <section className="flex w-[21rem] xl:w-[rem] pt-[2rem] xl:pt-[4.5rem] ">
+        <section className=" mt-20 ">
           <CarouselActivity slides={slides} options={OPTIONS} />
         </section>
       )}
